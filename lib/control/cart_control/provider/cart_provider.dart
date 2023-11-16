c7ad10d9ab89dd9559b4cfcbb195520fbb1684ff
sandeep abraham <sandeep.pggroup@gmail.com>
2023-11-16 20:12:30 +0530

    final before second build for open test

--- lib/control/cart_control/provider/cart_provider.dart
+++ lib/control/cart_control/provider/cart_provider.dart
@@ -3,7 +3,6 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:user_module/control/cart_control/service/cart_service.dart';
-import 'package:user_module/core/colors/colors.dart';
 import 'package:user_module/model/cart_model/cart_model.dart';
 
 class CartProvider extends ChangeNotifier {
--- lib/widget/payment_option.dart
+++ lib/widget/payment_option.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:user_module/control/place_order_payment_provider/provider/place_order_payment_provider.dart';
+import 'package:user_module/core/colors/colors.dart';
 
 Future<void> bottomSheetPaymentOption(BuildContext context, int cartSum) async {
   final placeOrderPaymentProvider =
       Provider.of<PlaceOrderPaymentProvider>(context, listen: false);
   return await showModalBottomSheet(
-    backgroundColor: Colors.black,
+    backgroundColor: Colors.grey,
     // barrierColor: Colors.blue,
     context: context,
     builder: (BuildContext context) {
@@ -32,6 +33,8 @@ Future<void> bottomSheetPaymentOption(BuildContext context, int cartSum) async {
                       placeOrderPaymentProvider.cashOnDelivery(
                           'COD', cartSum, context);
                     },
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: buttonColor),
                     child: const FaIcon(
                       FontAwesomeIcons.moneyBill,
                       size: 40,
@@ -54,6 +57,8 @@ Future<void> bottomSheetPaymentOption(BuildContext context, int cartSum) async {
                       placeOrderPaymentProvider.onlinePaymentBackend(
                           'Online', cartSum, context);
                     },
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: buttonColor),
                     child: const Icon(
                       Icons.credit_card,
                       size: 40,
